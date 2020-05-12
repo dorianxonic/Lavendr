e4a9be8487e7bd4fc302f868166adac3960e9564
dorianxonic <dimoviktor@gmail.com>
2020-05-12 13:14:24 +0200

    shellscript loop

---
+++ lavender.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while true; do
+echo Iniciando Lavender...
+sleep 3
+node ./src/lavender.js
+echo Lavender parado/crasheado - reiniciando en 5 segundos
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+done
